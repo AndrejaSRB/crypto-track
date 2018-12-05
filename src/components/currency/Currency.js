c8eb78958334b73715f2dc9e86cfce711b2bc7e7
AndrejaSRB <andreja.kojadinovic@gmail.com>
2018-12-05 22:24:44 +0100

    rendering table withAPI information, adding styles

--- src/components/currency/Currency.js
+++ src/components/currency/Currency.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Currency.css'
 
 class Currency extends Component {
     state = {
@@ -12,13 +13,53 @@ class Currency extends Component {
         fetch(url)
         .then(res => res.json())
         .then(res => {
-            this.setState({currencies: res.data})            
+            this.setState({currencies: res.data.slice(0,50)})            
         })
     };
-    render() { 
+    render() {          
         return ( 
-            <h1>Hello World</h1>
-         );
+            <table className="currency-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Short Name</th>
+                        <th>$ Value</th>
+                        <th>last 24h</th>
+                        <th>Amout you own</th>
+                        <th>$ value of your coin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.currencies.length ? (
+                    this.state.currencies.map(currency => {            
+                        return(
+                            <tr key={currency.id}>
+                                <td>{currency.name}</td>
+                                <td>{currency.symbol}</td>
+                                <td>{parseFloat(currency.quote.USD.price).toFixed(2)}</td>
+                                <td
+                                style={currency.quote.USD.percent_change_24h < 0 ? {color : 'red'}: {color:'green'}}
+                                >
+                                {parseFloat(currency.quote.USD.percent_change_24h).toFixed(2)} %
+                                </td>
+                                <td className="userValue">
+                                <input 
+                                className="currency-input" 
+                                type="number" 
+                                />
+                                <button 
+                                className="submit-btn" 
+                                >Submit</button>
+                                </td>
+                                <td>{currency.myCalculatedValueCoin || 0.00}</td>
+                            </tr>
+                        )
+                        })
+                    ) : (<tr className="loading"></tr>)
+                }
+                </tbody>
+            </table>   
+        );
     }
 }
  
